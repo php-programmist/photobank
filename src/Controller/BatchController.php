79dbe7abab73239f40005c25c24ad57f0d3c1475
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-11-23 16:35:07 +0300

    Разграничение доступа

--- src/Controller/BatchController.php
+++ src/Controller/BatchController.php
@@ -10,7 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * @Route("/batch")
+ */
 class BatchController extends AbstractController
 {
     /**
